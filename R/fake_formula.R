edb72d24e5d2c6927199a197902734c31c22d432
Nikolaus Umlauf <Nikolaus.Umlauf@uibk.ac.at>
2023-11-07 11:01:09 +0100

    Started fake_formula for processing GAMLSS formulas.

---
+++ R/fake_formula.R
@@ -0,0 +1,81 @@
+## Function takes a formula, Formula, or a list of formulas
+## and extracts only the necessary parts to create a model.frame,
+## as well as the parts that are needed to setup smooth term
+## specification lists, or any type of special model terms
+## that should be used for fitting.
+fake_formula <- function(formula, specials = NULL, nospecials = FALSE, onlyspecials = FALSE)
+{
+  if(is.list(formula))
+    formula <- do.call("as.Formula", formula)
+
+  if(any(grepl("|", as.character(formula), fixed = TRUE)))
+    formula <- as.Formula(formula)
+
+  n <- length(formula)
+
+  outer <- FALSE
+  if(length(n) > 1) {
+    if(n[2L] > 1)
+      outer <- TRUE
+  }
+
+  if(outer) {
+    fl <- list()
+    lhs <- formula(formula, rhs = 0)
+    for(i in 1:n[2L]) {
+      fl[[i]] <- fake_formula(formula(formula, rhs = i, lhs = 0), specials = specials,
+        nospecials = nospecials, onlyspecials = onlyspecials)
+    }
+    if(!is.character(fl[[1L]])) {
+      formula <- . ~ .
+      fl <- do.call("as.Formula", fl)
+      fl <- formula(fl, lhs = 0, drop = FALSE)
+      formula <- as.Formula(fl)
+      formula[[3L]] <- fl
+      attr(formula, "lhs") <- attr(as.Formula(lhs), "lhs")
+    } else {
+      formula <- fl
+    }
+  } else {
+    stn <- c("s", "te", "t2", "sx", "s2", "rs", "ti",
+      "tx", "tx2", "tx3", "tx4", "la", "n", "h", "lf",
+       "af", "lf.vd", "re", "peer", "fpc", "lin", "rb", "tree")
+    stn <- unique(c(stn, specials))
+
+    mt <- terms(formula, specials = stn)
+
+    os <- NULL
+    st <- unlist(attr(mt, "specials"))
+    if(!is.null(st)) {
+      tls <- rownames(attr(mt, "factors"))[st]
+      tls <- unique(tls)
+      ff <- NULL
+      for(j in tls) {
+        p <- parse(text = j)
+        v <- all.vars(p)
+        e <- eval(parse(text = paste0("quote(", j, ")")))
+        for(i in seq_along(e)) {
+          av <- all.vars(e[[i]])
+          av <- av[av != "|"]
+          if(length(av)) {
+            if(av %in% v)
+              ff <- c(ff, e[[i]])
+          }
+        }
+        os <- c(os, j)
+        eval(parse(text = paste0("formula <- update(formula, . ~ . -", j,")")))
+        if(!nospecials) {
+          for(i in ff) {
+            eval(parse(text = paste0("formula <- update(formula, . ~ . +", deparse(i),")")))
+          }
+        }
+      }
+    }
+
+    if(onlyspecials)
+      formula <- os
+  }
+
+  return(formula)
+}
+

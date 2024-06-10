a67056e7a293ab2b6d3b82dbbcc9700283cf5489
Nikolaus Umlauf <Nikolaus.Umlauf@uibk.ac.at>
2024-06-10 15:24:41 +0200

    using parallel with stepwise()

--- R/stepwise.R
+++ R/stepwise.R
@@ -58,8 +58,8 @@ forward_backward_step <- function(selected, notselected, xterms, sterms, strateg
   ## Get current iterator.
   iter <- length(stats_save)
 
-  ## Current GAIC.
-  gaic <- stats_save[[iter]]$GAIC
+  ## Multiple core processing?
+  cores <- control$cores
 
   ## Start select loop.
   do <- TRUE
@@ -68,7 +68,8 @@ forward_backward_step <- function(selected, notselected, xterms, sterms, strateg
   while(do && (k < maxit)) {
     if(length(modelterms) > 1L) {
       stats <- list()
-      for(j in modelterms) {
+
+      fitfun <- function(j) {
         ## Set model terms.
         xtermsj <- xterms
         stermsj <- sterms
@@ -92,16 +93,18 @@ forward_backward_step <- function(selected, notselected, xterms, sterms, strateg
         m <- RS(x, y, specials = specials, family, offsets,
           weights, start = start, xtermsj, sterms = stermsj, control)
 
-        ## Store stats.
-        stats[[j]] <- modelstats(m, K)
+        ## Return stats.
+        return(modelstats(m, K))
+      }
 
-        if(stats[[j]]$GAIC < gaic) {
-          model <- m
-          gaic <- stats[[j]]$GAIC
-        }
+      if(cores < 2L) {
+        stats <- lapply(modelterms, fitfun)
+      } else {
+        stats <- parallel::mclapply(modelterms, fitfun, mc.cores = cores)
       }
 
       ## Term with minimum GAIC?
+      names(stats) <- modelterms
       j <- sapply(stats, function(x) x$GAIC)
       j <- names(j)[which.min(j)]
 
@@ -163,6 +166,9 @@ forward_backward_step <- function(selected, notselected, xterms, sterms, strateg
     k <- k + 1L
   }
 
+  model <- RS(x, y, specials = specials, family, offsets,
+    weights, start = start, xterms, sterms = sterms, control)
+
   rval <- list("selected" = selected, "notselected" = notselected,
     "stats_save" = stats_save, "xterms" = xterms, "sterms" = sterms,
     "model" = model)
@@ -171,37 +177,40 @@ forward_backward_step <- function(selected, notselected, xterms, sterms, strateg
 }
 
 replace_step <- function(selected, notselected, xterms, sterms, stats_save,
-  x, y, specials, family, offsets, weights, start, control, trace, K)
+  x, y, specials, family, offsets, weights, start, control, trace, K, maxit = Inf)
 {
   ## Get current iterator.
   iter <- length(stats_save)
 
-  ## Current GAIC.
-  gaic <- stats_save[[iter]]$GAIC
+  ## Multiple core processing?
+  cores <- control$cores
 
   ## Start select loop.
   do <- TRUE
   model <- NULL
-  while(do) {
+  k <- 0L
+  while(do & (k < maxit)) {
     if(length(selected) > 1L) {
-      stats <- list()
-      k <- 1L
-      for(j in selected) {
-        for(i in notselected) {
-          ## Set model terms.
-          xtermsj <- xterms
-          stermsj <- sterms
+      stats <- NULL
 
+      ## Set model terms.
+      xtermsj <- xterms
+      stermsj <- sterms
+
+      for(j in selected) {
+        ## Drop model term.
+        if(linear(j)) {
+          xtermsj <- dropterm(j, xterms)
+        } else {
+          stermsj <- dropterm(j, sterms)
+        }
+
+        fitfun <- function(i) {
+          ## Add model term.
           if(linear(i)) {
             xtermsj <- addterm(i, xterms)
           } else {
             stermsj <- addterm(i, sterms)
-          }
-
-          if(linear(j)) {
-            xtermsj <- dropterm(j, xterms)
-          } else {
-            stermsj <- dropterm(j, sterms)
           }
 
           ## Estimate model.
@@ -209,17 +218,20 @@ replace_step <- function(selected, notselected, xterms, sterms, stats_save,
             weights, start = start, xtermsj, sterms = stermsj, control)
 
           ## Store stats.
-          stats[[k]] <- modelstats(m, K)
-          stats[[k]]$drop <- j
-          stats[[k]]$add <- i
+          ms <- modelstats(m, K)
+          ms$drop <- j
+          ms$add <- i
 
-          if(stats[[k]]$GAIC < gaic) {
-            model <- m
-            gaic <- stats[[k]]$GAIC
-          }
-
-          k <- k + 1L
+          return(ms)
         }
+
+        if(cores < 2L) {
+          si <- lapply(notselected, fitfun)
+        } else {
+          si <- parallel::mclapply(notselected, fitfun, mc.cores = cores)
+        }
+
+        stats <- c(stats, si)
       }
 
       ## Term with minimum GAIC?
@@ -273,7 +285,11 @@ replace_step <- function(selected, notselected, xterms, sterms, stats_save,
     } else {
       do <- FALSE
     }
+    k <- k + 1L
   }
+
+  model <- RS(x, y, specials = specials, family, offsets,
+    weights, start = start, xterms, sterms = sterms, control)
 
   rval <- list("selected" = selected, "notselected" = notselected,
     "stats_save" = stats_save, "xterms" = xterms, "sterms" = sterms,
@@ -348,6 +364,107 @@ both_step <- function(selected, notselected, xterms, sterms, stats_save,
   return(rval)
 }
 
+all_step <- function(selected, notselected, xterms, sterms, stats_save,
+  x, y, specials, family, offsets, weights, start, control, trace, K)
+{
+  trace2 <- trace
+  trace2[2L] <- FALSE
+
+  gaic <- stats_save[[length(stats_save)]]$GAIC
+  model <- NULL
+
+  do <- TRUE
+  while(do) {
+    fs <- forward_backward_step(selected, notselected, xterms, sterms,
+      strategy = "forward", stats_save, x, y, specials, family, offsets, weights, start,
+      control, trace2, K, maxit = 1L)
+
+    bs <- forward_backward_step(selected, notselected, xterms, sterms,
+      strategy = "backward", stats_save, x, y, specials, family, offsets, weights, start,
+      control, trace2, K, maxit = 1L)
+
+    rs <- replace_step(selected, notselected, xterms, sterms,
+      stats_save, x, y, specials = specials, family, offsets, weights, start,
+      control, trace, K, maxit = 1L)
+
+    i1 <- length(fs$stats_save)
+    i2 <- length(bs$stats_save)
+    i3 <- length(rs$stats_save)
+
+    fs_step <- (fs$stats_save[[i1]]$GAIC < bs$stats_save[[i2]]$GAIC) &&
+      (fs$stats_save[[i1]]$GAIC < rs$stats_save[[i3]]$GAIC) &&
+      (fs$stats_save[[i1]]$GAIC < gaic)
+
+    bs_step <- (bs$stats_save[[i2]]$GAIC < fs$stats_save[[i1]]$GAIC) &&
+      (bs$stats_save[[i2]]$GAIC < rs$stats_save[[i3]]$GAIC) &&
+      (bs$stats_save[[i2]]$GAIC < gaic)
+
+    rs_step <- (rs$stats_save[[i3]]$GAIC < fs$stats_save[[i1]]$GAIC) &&
+      (rs$stats_save[[i3]]$GAIC < bs$stats_save[[i2]]$GAIC) &&
+      (rs$stats_save[[i3]]$GAIC < gaic)
+
+    if(any(c(fs_step, bs_step, rs_step))) {
+      if(fs_step) {
+        ## Print info.
+        if(trace[2L]) {
+          j <- splitname(fs$stats_save[[i1]]$term)
+          cat("  GAIC = ", fmt(fs$stats_save[[i1]]$GAIC, width = 10, digits = 4),
+            " <+> parameter ", j[1L], ", term ", j[2L],  "\n", sep = "")
+        }
+        selected <- fs$selected
+        notselected <- fs$notselected
+        xterms <- fs$xterms
+        sterms <- fs$sterms
+        stats_save <- fs$stats_save
+        model <- fs$model
+        gaic <- fs$stats_save[[i1]]$GAIC
+      }
+
+      if(bs_step) {
+        ## Print info.
+        if(trace[2L]) {
+          j <- splitname(bs$stats_save[[i2]]$term)
+          cat("  GAIC = ", fmt(bs$stats_save[[i2]]$GAIC, width = 10, digits = 4),
+            " <-> parameter ", j[1L], ", term ", j[2L],  "\n", sep = "")
+        }
+        selected <- bs$selected
+        notselected <- bs$notselected
+        xterms <- bs$xterms
+        sterms <- bs$sterms
+        stats_save <- bs$stats_save
+        model <- bs$model
+        gaic <- bs$stats_save[[i2]]$GAIC
+      }
+
+      if(rs_step) {
+        ## Print info.
+        if(trace[2L]) {
+          r <- splitname(rs$stats_save[[i3]]$dropped)
+          a <- splitname(rs$stats_save[[i3]]$term)
+          cat("  GAIC = ", fmt(rs$stats_save[[i3]]$GAIC, width = 10, digits = 4),
+              " <-> parameter ", r[1L], ", term ", r[2L],  "\n", sep = "")
+          cat("                    <+> parameter ", a[1L], ", term ", a[2L],  "\n", sep = "")
+        }
+        selected <- rs$selected
+        notselected <- rs$notselected
+        xterms <- rs$xterms
+        sterms <- rs$sterms
+        stats_save <- rs$stats_save
+        model <- rs$model
+        gaic <- rs$stats_save[[i3]]$GAIC
+      }
+    } else {
+      do <- FALSE
+    }
+  }
+
+  rval <- list("selected" = selected, "notselected" = notselected,
+    "stats_save" = stats_save, "xterms" = xterms, "sterms" = sterms,
+    "model" = model)
+
+  return(rval)
+}
+
 stepwise <- function(x, y, specials, family, offsets, weights, start, xterms, sterms, control)
 {
   nx <- family$names
@@ -362,7 +479,7 @@ stepwise <- function(x, y, specials, family, offsets, weights, start, xterms, st
   ## Which strategies to use?
   strategy <- control$strategy
   choices <- c("forward.linear", "forward", "backward", "backward.linear",
-     "replace", "replace.linear", "both", "both.linear")
+     "replace", "replace.linear", "both", "both.linear", "all", "all.linear")
   if(is.null(strategy)) {
     strategy <- c("both.linear", "both")
   } else {
@@ -381,6 +498,11 @@ stepwise <- function(x, y, specials, family, offsets, weights, start, xterms, st
 
   ## Penalty for AIC.
   K <- if(is.null(control$K)) 2 else control$K
+
+  ## Parallel loops?
+  if(is.null(control$cores)) {
+    control$cores <- 1L
+  }
 
   ## Estimate nullmodel first.
   xterms_itcpt <- list()
@@ -477,6 +599,44 @@ stepwise <- function(x, y, specials, family, offsets, weights, start, xterms, st
       } else {
         ## Bidirectional selection.
         si <- both_step(selected, notselected, xterms, sterms = NULL,
+          stats_save, x, y, specials = NULL, family, offsets, weights, start, control, trace, K)
+
+        ## Update.
+        if(length(si$model)) {
+          selected <- si$selected
+          notselected <- si$notselected
+          xterms <- si$xterms
+          stats_save <- si$stats_save
+          model <- si$model
+        }
+      }
+    }
+
+    if("all.linear" %in% strategy) {
+      if(trace[2L] && (iter < 2L))
+        cat("All Directions Linear Selection\n")
+      if(keeporder) {
+        for(i in nx) {
+          ## Get only linear terms
+          isel <- grep(paste0(i, ".p."), selected, fixed = TRUE, value = TRUE)
+          inot <- grep(paste0(i, ".p."), notselected, fixed = TRUE, value = TRUE)
+
+          ## Run bidirectional selection.
+          si <- all_step(isel, inot, xterms, sterms = NULL,
+            stats_save, x, y, specials = NULL, family, offsets, weights, start, control, trace, K)
+
+          ## Update.
+          if(length(si$model)) {
+            selected <- c(selected[!grepl(paste0(i, ".p."), selected, fixed = TRUE)], si$selected)
+            notselected <- c(notselected[!grepl(paste0(i, ".p."), notselected, fixed = TRUE)], si$notselected)
+            xterms <- si$xterms
+            stats_save <- si$stats_save
+            model <- si$model
+          }
+        }
+      } else {
+        ## All directions selection.
+        si <- all_step(selected, notselected, xterms, sterms = NULL,
           stats_save, x, y, specials = NULL, family, offsets, weights, start, control, trace, K)
 
         ## Update.
@@ -692,6 +852,56 @@ stepwise <- function(x, y, specials, family, offsets, weights, start, xterms, st
       }
     }
 
+    if("all" %in% strategy) {
+      if(trace[2L] && (iter < 2L))
+        cat("All Directions Selection\n")
+      if(keeporder) {
+        for(i in nx) {
+          ## Get selected terms.
+          isel <- c(
+            grep(paste0(i, ".p."), selected, fixed = TRUE, value = TRUE),
+            grep(paste0(i, ".s."), selected, fixed = TRUE, value = TRUE)
+          )
+
+          ## Get not selected terms.
+          inot <- c(
+            grep(paste0(i, ".p."), notselected, fixed = TRUE, value = TRUE),
+            grep(paste0(i, ".s."), notselected, fixed = TRUE, value = TRUE)
+          )
+
+          ## Run bidirectional selection.
+          si <- all_step(isel, inot, xterms, sterms,
+            stats_save, x, y, specials, family, offsets, weights, start, control, trace, K)
+
+          ## Update.
+          if(length(si$model)) {
+            jd <- !grepl(paste0(i, ".p."), selected, fixed = TRUE) & !grepl(paste0(i, ".s."), selected, fixed = TRUE)
+            selected <- c(selected[jd], si$selected)
+            jd <- !grepl(paste0(i, ".p."), notselected, fixed = TRUE) & !grepl(paste0(i, ".s."), notselected, fixed = TRUE)
+            notselected <- c(notselected[jd], si$notselected)
+            xterms <- si$xterms
+            sterms <- si$sterms
+            stats_save <- si$stats_save
+            model <- si$model
+          }
+        }
+      } else {
+        ## Bidirectional selection.
+        si <- all_step(selected, notselected, xterms, sterms,
+          stats_save, x, y, specials, family, offsets, weights, start, control, trace, K)
+
+        ## Update.
+        if(length(si$model)) {
+          selected <- si$selected
+          notselected <- si$notselected
+          xterms <- si$xterms
+          sterms <- si$sterms
+          stats_save <- si$stats_save
+          model <- si$model
+        }
+      }
+    }
+
     if("replace" %in% strategy) {
       if(trace[2L] && (iter < 2L))
         cat("Replace Selection\n")
@@ -832,8 +1042,10 @@ xs2formula <- function(x, s)
 }
 
 stepGAMLSS <- function(formula, ..., K = 2,
-  strategy = c("both.linear", "both"), keeporder = FALSE)
+  strategy = c("both.linear", "both"), keeporder = FALSE,
+  cores = 1L)
 {
-  gamlss2(formula, ..., optimizer = stepwise, K = K, strategy = strategy, keeporder = keeporder)
+  gamlss2(formula, ..., optimizer = stepwise, K = K, strategy = strategy,
+    keeporder = keeporder, cores = cores)
 }
 
